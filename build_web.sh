e42dfc647f9e53435e592056b29bdeba76096144
RussellBentley <russell.w.bentley@icloud.com>
2020-07-09 01:37:00 -0400

    Rename to demo_072020

--- build_web.sh
+++ build_web.sh
@@ -5,4 +5,4 @@ mkdir -p target/web
 
 cp src_web/index.html src_web/gl.js src_web/live.js target/web
 cargo build --release --target wasm32-unknown-unknown
-cp target/wasm32-unknown-unknown/release/web-tetris.wasm target/web
+cp target/wasm32-unknown-unknown/release/demo_072020.wasm target/web
