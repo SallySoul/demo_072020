7dfcab708a55382904c375b53640ef59622a74cf
RussellBentley <russell.w.bentley@icloud.com>
2020-07-09 03:12:15 -0400

    Try adding explore

--- build_web.sh
+++ build_web.sh
@@ -3,6 +3,6 @@
 rm -rf target/web
 mkdir -p target/web
 
-cp src_web/index.html src_web/gl.js src_web/live.js target/web
+cp src_web/index.html src_web/gl.js target/web
 cargo build --release --target wasm32-unknown-unknown
 cp target/wasm32-unknown-unknown/release/demo_072020.wasm target/web
